e44a13dcafdd8563195180dc90a70c04c36d1613
Skylyxx <skylyxxfr@gmail.com>
2021-04-19 18:44:51 +0200

    :art: Make logging coloured

--- src/main/java/fr/skylyxx/docsgenerator/Logger.java
+++ src/main/java/fr/skylyxx/docsgenerator/Logger.java
@@ -1,16 +1,18 @@
 package fr.skylyxx.docsgenerator;
 
+import org.bukkit.ChatColor;
+
 import java.util.logging.Level;
 
 public class Logger {
 
-    private static SkriptDocsGenerator skriptDocsGenerator = SkriptDocsGenerator.getPlugin(SkriptDocsGenerator.class);
+    private static final SkriptDocsGenerator skriptDocsGenerator = SkriptDocsGenerator.getPlugin(SkriptDocsGenerator.class);
 
     public static void log(Level level, String msg, String... args) {
         for (int i = 0; i < args.length; i++) {
             msg = msg.replace("{" + i + "}", args[i]);
         }
-        skriptDocsGenerator.getLogger().log(level, msg);
+        skriptDocsGenerator.getLogger().log(level, ChatColor.translateAlternateColorCodes('&', msg));
     }
 
     public static void info(String msg, String... args) {
